b70465a98f65d76b2f37c0f9fb9705a8c8c084f2
wangpatrick57 <wang.patrick57@gmail.com>
2023-04-11 17:14:52 -0700

    finished testing multithreaded dangling reference

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,20 +1,38 @@
 #include <iostream>
 #include <thread>
+#include <mutex>
+#include <unistd.h>
+
+std::mutex cout_mutex;
 
 class hello {
 private:
-    int i;
+    int &i;
 public:
-    hello(int i_):i(i_){}
+    hello(int &i_):i(i_){}
     void operator()() const {
-        for (int j = 0; j < 100000; j++) {
-            std::cout << "hello world " << i << std::endl;
+        for (int j = 0; j < 10; j++) {
+            usleep(100000 + i);
+            cout_mutex.lock();
+            std::cout << j << " hello world " << i << std::endl;
+            cout_mutex.unlock();
         }
     }
 };
 
+void except() {
+    int tmp = 3;
+    std::thread t{hello(tmp)};
+    t.detach();
+    cout_mutex.lock();
+    std::cout << "exiting oops" << std::endl;
+    cout_mutex.unlock();
+}
+
 int main() {
-    std::thread t((hello(5)));
-    t.join();
-    std::cout << "end\n";
+    except();
+    sleep(2);
+    cout_mutex.lock();
+    std::cout << "exiting main" << std::endl;
+    cout_mutex.unlock();
 }
