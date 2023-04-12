ae722d245197840579a46b509441b16f4fe3459c
wangpatrick57 <wang.patrick57@gmail.com>
2023-04-12 10:56:44 -0700

    finished mutex crawl

--- web_crawler.cpp
+++ web_crawler.cpp
@@ -1,8 +1,43 @@
 #include "web_crawler.hpp"
 
-void serial_crawl(std::string start_url, std::unordered_map<std::string, bool>& fetched) {
-    bool already_fetched = fetched.find(start_url) != fetched.end() && fetched.at(start_url);
-    fetched[start_url] = true;
+std::unordered_map<std::string, FetchResult> const FAKE = {
+    {"http://golang.org/", {
+        "The Go Programming Language",
+        {
+            "http://golang.org/pkg/",
+            "http://golang.org/cmd/"
+        }
+    }},
+    {"http://golang.org/pkg/", {
+        "Packages",
+        {
+            "http://golang.org/",
+            "http://golang.org/cmd/",
+            "http://golang.org/pkg/fmt/",
+            "http://golang.org/pkg/os/"
+        }
+    }},
+    {"http://golang.org/pkg/fmt/", {
+        "Package fmt",
+        {
+            "http://golang.org/",
+            "http://golang.org/pkg/"
+        }
+    }},
+    {"http://golang.org/pkg/os/", {
+        "Package os",
+        {
+            "http://golang.org/",
+            "http://golang.org/pkg/"
+        }
+    }}
+};
+
+void mutex_crawl(std::string start_url, MutexFetched& mf) {
+    mf.mu.lock();
+    bool already_fetched = mf.fetched.find(start_url) != mf.fetched.end() && mf.fetched.at(start_url);
+    mf.fetched[start_url] = true;
+    mf.mu.unlock();
 
     if (already_fetched) {
         return;
@@ -10,55 +45,55 @@ void serial_crawl(std::string start_url, std::unordered_map<std::string, bool>& 
 
     try {
         FetchResult res = fetch(start_url);
+        cout_mutex.lock();
         std::cout << "fetched body " << res.body << std::endl;
+        cout_mutex.unlock();
+        std::vector<std::thread> threads;
 
         for (std::string url : res.urls) {
-            serial_crawl(url, fetched);
+            threads.emplace_back(mutex_crawl, url, std::ref(mf));
+        }
+
+        for (std::thread& t : threads) {
+            t.join();
         }
     } catch (const std::out_of_range& e) {
+        cout_mutex.lock();
         std::cout << "didn't find " << start_url << std::endl;
+        cout_mutex.unlock();
+    }
+}
+
+void serial_crawl(std::string start_url, MutexFetched& mf) {
+    bool already_fetched = mf.fetched.find(start_url) != mf.fetched.end() && mf.fetched.at(start_url);
+    mf.fetched[start_url] = true;
+
+    if (already_fetched) {
+        return;
+    }
+
+    try {
+        FetchResult res = fetch(start_url);
+        cout_mutex.lock();
+        std::cout << "fetched body " << res.body << std::endl;
+        cout_mutex.unlock();
+
+        for (std::string url : res.urls) {
+            serial_crawl(url, mf);
+        }
+    } catch (const std::out_of_range& e) {
+        cout_mutex.lock();
+        std::cout << "didn't find " << start_url << std::endl;
+        cout_mutex.unlock();
     }
 }
 
 FetchResult fetch(std::string url) {
-    std::unordered_map<std::string, FetchResult> fake {
-        {"http://golang.org/", {
-            "The Go Programming Language",
-            {
-                "http://golang.org/pkg/",
-			    "http://golang.org/cmd/"
-            }
-        }},
-        {"http://golang.org/pkg/", {
-            "Packages",
-            {
-                "http://golang.org/",
-			    "http://golang.org/cmd/",
-			    "http://golang.org/pkg/fmt/",
-			    "http://golang.org/pkg/os/"
-            }
-        }},
-        {"http://golang.org/pkg/fmt/", {
-            "Package fmt",
-            {
-			    "http://golang.org/",
-                "http://golang.org/pkg/"
-            }
-        }},
-        {"http://golang.org/pkg/os/", {
-            "Package os",
-            {
-			    "http://golang.org/",
-                "http://golang.org/pkg/"
-            }
-        }}
-    };
-
     usleep(500000);
-    return fake.at(url);
+    return FAKE.at(url);
 }
 
 int main() {
-    std::unordered_map<std::string, bool> fetched;
-    serial_crawl("http://golang.org/", fetched);
+    MutexFetched mf{};
+    mutex_crawl("http://golang.org/", mf);
 }
--- web_crawler.hpp
+++ web_crawler.hpp
@@ -5,13 +5,29 @@
 #include <unistd.h>
 #include <unordered_map>
 #include <vector>
+#include <mutex>
+#include <thread>
 
 typedef struct {
     std::string body;
     std::vector<std::string> urls;
 } FetchResult;
 
-void serial_crawl(std::string start_url);
+typedef struct {
+    std::mutex mu;
+    std::unordered_map<std::string, bool> fetched;
+} MutexFetched;
+
+typedef struct {
+    int a;
+    bool b;
+} Test;
+
+extern std::unordered_map<std::string, FetchResult> const FAKE;
+std::mutex cout_mutex{};
+
+void mutex_crawl(std::string start_url, MutexFetched& mf);
+void serial_crawl(std::string start_url, MutexFetched& mf);
 FetchResult fetch(std::string url);
 
 #endif
